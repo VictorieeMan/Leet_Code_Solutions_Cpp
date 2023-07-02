271bcf044e5198c26d5bbb89011fe1bb8aaac922
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-02 14:51:14 +0200

    Added template file

---
+++ @template.cpp
@@ -0,0 +1,18 @@
+//Created: 2023- by VictorieeMan
+/*
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+class Solution {
+public:
+
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
