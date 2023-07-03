7529c7589eb6345d4774fc4c08fc233f88329dcd
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-03 14:07:33 +0200

    Added new exercise

---
+++ invert-binary-tree.cpp
@@ -0,0 +1,18 @@
+//Created: 2023-7-03 by VictorieeMan
+/* https://leetcode.com/problems/invert-binary-tree/
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+class Solution {
+public:
+
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
