88e51755893f79600bdb0bd7aaa0b3b25efe00ce
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-04 16:27:46 +0200

    started new exercise

---
+++ best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,22 @@
+//Created: 2023-07-04 by VictorieeMan
+/* https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
