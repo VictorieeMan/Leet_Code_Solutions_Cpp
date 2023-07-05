acec832d071fb2f0fae79927af65291212cf87e5
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-05 13:04:48 +0200

    solved

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -1,5 +1,12 @@
 //Created: 2023-07-04 by VictorieeMan
 /* https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+Runtime 139 ms
+Beats
+81.29%
+
+Memory 93.3 MB
+Beats
+72.86%
 */
 #include <iostream>
 #include <vector>
@@ -12,34 +19,21 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<pair<int,int>> price_days;
+        //Greedy algorithm, doing tests along the iteration taking best found.
+        int buy = prices[0];
+        int max_profit = 0;
         int days = prices.size();
-        for(int i = 0; i < days; ++i){
-            price_days.push_back(make_pair(prices[i],i));
-        }
-
-        sort(price_days.begin(),price_days.end());
-        vector<int> diffs(1,0);
-
-        int iterations = 0;
-        for(int i = 0; i < days; ++i){
-            for(int j = days-1; j > i; --j){
-                if(price_days[i].second < price_days[j].second){
-                    int diff = price_days[j].first - price_days[i].first;
-                    cout << diff << " ";
-                    if(diff <= 0){
-                        break;
-                    }
-                    diffs.push_back(diff);
+        for(int i = 1; i < days; ++i){
+            int today = prices[i];
+            if(buy < today){
+                if(int profit = today-buy; profit > max_profit){
+                    max_profit = profit;
                 }
-                ++iterations;
+            } else if(buy > today){
+                buy = today;
             }
         }
-
-        auto it = max_element(diffs.begin(),diffs.end());
-
-        cout << ", " << iterations << endl;
-        return *it;
+        return max_profit;
     }
 };
 
